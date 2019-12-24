7f1ea50dd78c1e96682f53f0ac5c9ce27aa5f12f
Sami Honkanen <sami.honkanen@live.fi>
2019-12-24 12:36:19 +0200

    Added local settings to use postgresql and changed localization

--- contactmemng/contactmemng/settings.py
+++ contactmemng/contactmemng/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Load local settings
+try:
+    from .local_settings import *
+except ImportError:
+    pass
