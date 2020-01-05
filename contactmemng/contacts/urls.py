655e6ac7e81b1d53b2155f2d5ae34b85360b4ab3
Sami Honkanen <sami.honkanen@live.fi>
2020-01-05 14:03:58 +0200

    Forgot to change route

--- contactmemng/contacts/urls.py
+++ contactmemng/contacts/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import ContactViewset
 
 router = routers.DefaultRouter()
-router.register('api/contacts', ContactViewset, 'contacts')
+router.register('contacts', ContactViewset, 'contacts')
 
 urlpatterns = router.urls
