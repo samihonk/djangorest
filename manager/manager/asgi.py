a065bef36f6bc278a7a9b57647f81ea954be578b
Sami Honkanen <sami.honkanen@live.fi>
2020-02-25 14:52:03 +0200

    Renaming continued

--- manager/manager/asgi.py
+++ manager/manager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for contactmemng project.
+ASGI config for manager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactmemng.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')
 
 application = get_asgi_application()
